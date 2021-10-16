54ad157b88f207014185b120fc533ea528fdafe7
kbanchi1111 <kbanchi.1111@gmail.com>
2021-10-16 09:10:49 +0900

    add masonry

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Article extends Model
 {
     use HasFactory;
+
+    public function attachment()
+    {
+        return $this->hasOne(Attachment::class);
+    }
+
+    public function getImagePathAttribute()
+    {
+        return 'articles/' . $this->attachment->name;
+    }
+
+    public function getImageUrlAttribute()
+    {
+        return Storage::url($this->image_path);
+    }
 }
